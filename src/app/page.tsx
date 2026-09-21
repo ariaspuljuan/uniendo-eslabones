ff6ed4c3249f0d2399c188ac4ccb6e7b47165239
JuanSebastian <ariaspulidojuan@gmail.com>
2026-09-21 11:11:29 -0500

    Desactivar temporalmente ventana emergente

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,10 @@ import { Hero } from "@/components/home/Hero";
 import { ImpactSection } from "@/components/home/ImpactSection";
 import { LatestNews } from "@/components/home/LatestNews";
 import { QuickAccess } from "@/components/home/QuickAccess";
-import { AnnouncementModal } from "@/components/home/AnnouncementModal";
-import { urgentAnnouncement } from "@/data/news";
 
 export default function HomePage() {
   return (
     <main>
-      <AnnouncementModal announcement={urgentAnnouncement} />
       <Hero />
       <FeaturedProducts />
       <QuickAccess />
